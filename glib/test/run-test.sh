48e7d8b2752ae33b7bb7567ec3325b71e549fd1f
Kouhei Sutou <kou@cozmixng.org>
2008-10-12 13:00:14 +0900

    * use --keep-opening-modules.

--- glib/test/run-test.sh
+++ glib/test/run-test.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 export BASE_DIR="`dirname $0`"
+top_dir="$BASE_DIR/../.."
 
 if test x"$NO_MAKE" != x"yes"; then
-    make -C $BASE_DIR/../../ > /dev/null || exit 1
+    make -C "$top_dir" > /dev/null || exit 1
 fi
 
 if test -z "$CUTTER"; then
@@ -11,10 +12,15 @@ if test -z "$CUTTER"; then
 fi
 
 CUTTER_WRAPPER=
+CUTTER_ARGS=
 if test x"$CUTTER_DEBUG" = x"yes"; then
-    CUTTER_WRAPPER="$BASE_DIR/../../libtool --mode=execute gdb --args"
+    CUTTER_WRAPPER="$top_dir/libtool --mode=execute gdb --args"
+    CUTTER_ARGS="--keep-opening-modules"
 fi
 
-CUTTER_ARGS="-s $BASE_DIR --exclude-directory lib"
+export CUTTER
+
+CUTTER_ARGS="$CUTTER_ARGS -s $BASE_DIR"
+CUTTER_ARGS="$CUTTER_ARGS --exclude-directory lib"
 CUTTER_ARGS="$CUTTER_ARGS --exclude-directory fixtures"
 $CUTTER_WRAPPER $CUTTER $CUTTER_ARGS "$@" $BASE_DIR
